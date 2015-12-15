bf60847192bd50ed8d8e5832ecf03b43a7f305d2
Adam Morgan <atom.morgan@gmail.com>
2015-12-15 17:55:19 -0500

    Add Protractor and first E2E test

---
+++ app/sections/home/home.e2e.js
@@ -0,0 +1,6 @@
+describe("Home Page", function() {
+  it("should default to home state", function() {
+    browser.get("http://localhost:8080/#/");
+    expect(browser.getCurrentUrl()).to.eventually.equal("http://localhost:8080/#/");
+  });
+});
