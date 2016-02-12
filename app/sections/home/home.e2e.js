0190ea94bfb20ac4007dc85434a0ea2d7658110d
Adam Morgan <atom.morgan@gmail.com>
2016-02-12 13:31:56 -0500

    Update sample E2E test for base url

--- app/sections/home/home.e2e.js
+++ app/sections/home/home.e2e.js
@@ -1,7 +1,7 @@
 describe("Home Page", function() {
   it("should default to home state", function() {
-    browser.get("http://localhost:8080/#/");
-    expect(browser.getCurrentUrl()).to.eventually.equal("http://localhost:8080/#/");
+    browser.get("http://localhost:8080/");
+    expect(browser.getCurrentUrl()).to.eventually.equal("http://localhost:8080/");
     expect(browser.getTitle()).to.eventually.equal("TicketWin");
   });
 });
